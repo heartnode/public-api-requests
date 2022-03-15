5e88b8c4b2ef1b9f3191b917e0b48cd0a78f058f
heartnode <weinianhe@gmail.com>
2022-03-14 18:37:04 -0700

    Updated app to show first and last name

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function makeGallery(user){
             <img class="card-img" src="${user.picture.medium}" alt="profile picture">
         </div>
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${user.name.first}</h3>
+            <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
             <p class="card-text">${user.email}</p>
             <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
         </div>
